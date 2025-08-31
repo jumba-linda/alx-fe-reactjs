27076b041b294c787be7b384e93c5ac4ec3da90c
linda jumba <khavugwilinda@gmail.com>
2025-08-31 11:57:53 +0300

    adjustments

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,24 +1,17 @@
-// src/App.jsx
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile from './components/UserProfile';
-
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
-return (
-<>
-<Header />
-<MainContent />
-<Footer />
-
-
-{/* Task 3: User profile using props */}
-<UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
-</>
-);
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </div>
+  )
 }
 
-
-export default App;
+export default App
