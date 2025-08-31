bce135f204f6257cc158cca3e4581a76bab916fe
linda jumba <khavugwilinda@gmail.com>
2025-08-31 12:21:45 +0300

    change

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,13 +1,12 @@
 // src/components/UserProfile.jsx
 const UserProfile = (props) => {
-return (
-<div className="user-card">
-<h2>{props.name}</h2>
-<p>Age: {props.age}</p>
-<p>{props.bio}</p>
-</div>
-);
+  return (
+    <div className="user-card">
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>   {/* <-- Now includes 'Bio:' */}
+    </div>
+  );
 };
-
 
 export default UserProfile;
