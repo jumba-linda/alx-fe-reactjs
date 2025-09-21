6b6d4c2495d295d66d50e6ac34108a6617bb9f20
linda jumba <khavugwilinda@gmail.com>
2025-09-21 23:08:23 +0300

    wixxy

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 
+// Basic user fetch by username
 export const fetchUserData = async (username) => {
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
+};
+
+// Advanced user search with filters
+export const fetchAdvancedUsers = async (username, location, minRepos) => {
+  let query = username;
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>${minRepos}`;
+
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  return response.data.items;
 };
