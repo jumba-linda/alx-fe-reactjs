843618cad8102775b0e4165f3462c849e9a7ca01
linda jumba <khavugwilinda@gmail.com>
2025-09-28 16:34:58 +0300

    commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
       </h1>
       <p className="text-sm sm:text-sm md:text-base text-gray-600">
